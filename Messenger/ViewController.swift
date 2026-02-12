8d03d9ca8e1c13a5b69eabbf728bd49148437231
alekseiypetrov <aleshapetrov777@mail.ru>
2026-02-12 17:25:41 +0300

    Создан проект

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Алексей Петров on 12.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
