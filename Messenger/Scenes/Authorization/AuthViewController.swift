55c81af21569cc0aa582cbbe24b1a2cbb52883c3
alekseiypetrov <aleshapetrov777@mail.ru>
2026-02-13 21:34:50 +0300

    feat(ui): добавлены необходимые ui-элементы для экрана авторизации

--- Messenger/Scenes/Authorization/AuthViewController.swift
+++ Messenger/Scenes/Authorization/AuthViewController.swift
@@ -1,3 +1,54 @@
 import UIKit
 
-final class AuthViewController: UIViewController { }
+final class AuthViewController: UIViewController {
+    
+    // MARK: - UI-elements
+    
+    private lazy var imageView: UIView = {
+        let view = LogoScreenView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var greetingLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var hintLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var loginField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    private lazy var passwordField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    private lazy var logInButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var forgotPasswordButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var registrationButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+}
