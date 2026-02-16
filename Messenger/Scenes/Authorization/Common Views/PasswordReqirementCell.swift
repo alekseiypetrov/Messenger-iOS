0f490bca8ae7f183ba8898d279c62b5243447e7d
alekseiypetrov <aleshapetrov777@mail.ru>
2026-02-16 16:50:28 +0300

    feat(ui): выполнена верстка формы с требованиями для нового пароля при восстановлении доступа к аккаунту

---
+++ Messenger/Scenes/Authorization/Common Views/PasswordReqirementCell.swift
@@ -0,0 +1,82 @@
+import UIKit
+
+final class PasswordReqirementCell: UITableViewCell {
+    
+    // MARK: - Static Properties
+    
+    static let identifier = "PasswordReqirementCell"
+    
+    // MARK: - UI-elements
+    
+    private lazy var statusView: UIImageView = UIImageView(image: .filledCircleIcon)
+    
+    private lazy var title: UILabel = {
+        let label = UILabel()
+        label.font = .plainText2
+        label.textColor = .mText
+        label.textAlignment = .left
+        return label
+    }()
+    
+    // MARK: - Public properties
+    
+    var image: UIImage {
+        didSet {
+            switch image {
+            case .filledCircleIcon:
+                title.textColor = .mText
+            case .successCircleIcon:
+                title.textColor = .mDarkGreen
+            default:
+                break
+            }
+        }
+    }
+    
+    var text: String {
+        didSet {
+            title.text = text
+        }
+    }
+    
+    // MARK: - Initializers
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        image = .filledCircleIcon
+        text = ""
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) { nil }
+    
+    // MARK: - Lifecycle
+    
+    override func prepareForReuse() {
+        image = .filledCircleIcon
+    }
+    
+    // MARK: - Private methods
+    
+    private func setupView() {
+        contentView.addSubviews([statusView, title])
+    }
+    
+    private func setupConstraints() {
+        statusView.constraintEqualSides(withSize: 16.0)
+        
+        NSLayoutConstraint.activate([
+            
+            // Status View Constraints
+            statusView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            statusView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            
+            // Title Label Constraints
+            title.leadingAnchor.constraint(equalTo: statusView.trailingAnchor, constant: 8.0),
+            title.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            title.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            title.heightAnchor.constraint(equalToConstant: 20.0),
+        ])
+    }
+}
---
+++ Messenger/Scenes/Authorization/Common Views/PasswordRequirementsView.swift
@@ -0,0 +1,91 @@
+import UIKit
+
+final class PasswordRequirementsView: UIView {
+    
+    // MARK: - Constants
+    
+    private enum Constants {
+        static let cornerRadius: CGFloat = 14.0
+        static let cellTitles = [
+            "Минимум 8 символов",
+            "Пароли совпадают",
+        ]
+    }
+    
+    // MARK: - UI-elements
+    
+    private lazy var title: UILabel = {
+        let label = UILabel()
+        label.text = "Требования к паролю:"
+        label.font = .plainText2
+        label.textAlignment = .left
+        label.textColor = .mText
+        return label
+    }()
+    
+    private lazy var tableView: UITableView = {
+        let table = UITableView()
+        table.rowHeight = 24.0
+        table.allowsSelection = false
+        table.isScrollEnabled = false
+        table.separatorStyle = .none
+        table.dataSource = self
+        table.register(PasswordReqirementCell.self, forCellReuseIdentifier: PasswordReqirementCell.identifier)
+        return table
+    }()
+    
+    // MARK: - Initializers
+    
+    init() {
+        super.init(frame: .zero)
+        setupView()
+        setupConstrains()
+    }
+    
+    required init?(coder: NSCoder) { nil }
+    
+    // MARK: - Private methods
+    
+    private func setupView() {
+        backgroundColor = .white
+        layer.cornerRadius = Constants.cornerRadius
+        clipsToBounds = true
+        addSubviews([title, tableView])
+    }
+    
+    private func setupConstrains() {
+        NSLayoutConstraint.activate([
+            
+            // Title Label Constraints
+            title.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16.0),
+            title.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16.0),
+            title.topAnchor.constraint(equalTo: topAnchor, constant: 16.0),
+            title.heightAnchor.constraint(equalToConstant: 20.0),
+            
+            // TableView Constrains
+            tableView.leadingAnchor.constraint(equalTo: title.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: title.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 8.0),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16.0),
+        ])
+    }
+}
+
+extension PasswordRequirementsView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        Constants.cellTitles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: PasswordReqirementCell.identifier) as? PasswordReqirementCell
+        else { return UITableViewCell() }
+        cell.text = Constants.cellTitles[indexPath.row]
+        return cell
+    }
+    
+    func updateCell(at row: Int, isCompleted: Bool) {
+        let indexPath = IndexPath(row: row, section: 0)
+        guard let cell = tableView.cellForRow(at: indexPath) as? PasswordReqirementCell else { return }
+        cell.image = isCompleted ? .successCircleIcon : .filledCircleIcon
+    }
+}
